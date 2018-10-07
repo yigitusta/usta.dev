1b60c0c2bd58d3643df754c77b7776040306678e
Yigit Usta <yigitusta@pm.me>
2018-10-08 00:39:23 +0300

    Change hello and yigit to japanese

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+  const promises = [waitForDom(), getCountry()];
+  Promise.all(promises)
+    .then(function(results) {
+      const country = results.find(function(result) {
+        return typeof result === "object" && result.country;
+      }).country;
+      internationalization(country);
+    })
+    .catch(function(error) {
+      console.error(error[0]);
+    });
+
+  function internationalization(country) {
+    const hello = document.querySelector(".hello");
+
+    switch (country) {
+      case "japan":
+        const name = document.querySelector(".name");
+        hello.innerText = "こんにちは";
+        name.innerText = "MY NAME IS " + "イート。";
+        break;
+    }
+  }
+
+  window.sayHello = internationalization;
+
+  function waitForDom() {
+    return new Promise(function(resolve) {
+      document.addEventListener("DOMContentLoaded", resolve());
+    });
+  }
+
+  function getCountry() {
+    return new Promise(function(resolve, reject) {
+      fetch("http://ip-api.com/json/")
+        .then(function(result) {
+          return result.json();
+        })
+        .then(function(result) {
+          resolve({ country: result.country.toLowerCase() });
+        })
+        .catch(function(err) {
+          reject("Error fetching ip address and location data.\n" + err);
+        });
+    });
+  }
+})();
