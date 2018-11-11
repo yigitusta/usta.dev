e283b5a93d2dfb5328407bd095398bda011f9f70
Yigit Usta <yigitusta@jotform.com>
2018-11-11 14:39:00 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
 (function() {
-  const promises = [waitForDom(), getCountry()];
-  Promise.all(promises)
-    .then(function(results) {
-      const country = results.find(function(result) {
-        return typeof result === "object" && result.country;
-      }).country;
-      internationalization(country);
-    })
-    .catch(function(error) {
-      console.error(error[0]);
-    });
+  (function init() {
+    triggerChangeHello();
+    changeIntroPeriodically();
+  })();
 
-  function internationalization(country) {
-    const hello = document.querySelector(".hello");
+  function triggerChangeHello() {
+    const promises = [waitForDom(), getCountry()];
+    Promise.all(promises)
+      .then(function(results) {
+        const country = results.find(function(result) {
+          return typeof result === "object" && result.country;
+        }).country;
+        changeHelloText(country);
+      })
+      .catch(function(error) {
+        console.warn(error[0]);
+      });
 
-    switch (country) {
-      case "japan":
-        const name = document.querySelector(".name");
-        hello.innerText = "こんにちは";
-        name.innerText = "MY NAME IS " + "イート。";
-        break;
-        case "turkey":
-        hello.innerText = "HELLO";
-        break;
+    function changeHelloText(country) {
+      const hello = document.querySelector(".hello");
+      const name = document.querySelector(".name");
+
+      switch (country) {
+        case "japan":
+          hello.innerText = "こんにちは";
+          name.innerText = "MY NAME IS " + "イート。";
+          break;
+          case "turkey":
+          hello.innerText = "HELLO";
+          break;
+          case "finland":
+          name.innerText = "MY NAME IS " + "JIIT.";
+          hello.innerText = "Hei!"
+          break;
+          case "estonia":
+          hello.innerText = "Tere!";
+          break;
+      }
+      window.sayHello = changeHelloText;
+    }
+
+    function getCountry() {
+      return new Promise(function(resolve, reject) {
+        fetch("https://extreme-ip-lookup.com/json/")
+          .then(function(result) {
+            return result.json();
+          })
+          .then(function(result) {
+            resolve({ country: result.country.toLowerCase() });
+          })
+          .catch(function(err) {
+            reject("Error fetching ip address and location data.\n" + err);
+          });
+      });
     }
   }
-
-  window.sayHello = internationalization;
 
   function waitForDom() {
     return new Promise(function(resolve) {
@@ -34,18 +62,13 @@
     });
   }
 
-  function getCountry() {
-    return new Promise(function(resolve, reject) {
-      fetch("https://extreme-ip-lookup.com/json/")
-        .then(function(result) {
-          return result.json();
-        })
-        .then(function(result) {
-          resolve({ country: result.country.toLowerCase() });
-        })
-        .catch(function(err) {
-          reject("Error fetching ip address and location data.\n" + err);
-        });
-    });
+  function changeIntroPeriodically() {
+    const intro = document.querySelector('.intro');
+    const whatAmI = ['a Full-Stack Developer', 'a Senior University Student', 'an Amateur Rapper', 'a Traveler', 'a Software Engineer'];
+    let index = 0;
+    setInterval(function() {
+      intro.innerText = ('I\'m ' + whatAmI[index]).toUpperCase() + '.';
+      index = (index + 1) % whatAmI.length;
+    }, 2500);
   }
 })();
