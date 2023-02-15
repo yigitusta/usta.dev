0447c5bae332c128538a0c96211502ac806672e6
usta <yigitusta8@gmail.com>
2023-02-15 11:11:01 +0900

    add tailwind

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -2,7 +2,6 @@ import ReactDOMServer from "react-dom/server";
 import React from "react";
 import { PageShell } from "./PageShell";
 import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr";
-import logoUrl from "./logo.svg";
 import type { PageContextServer } from "./types";
 
 export { render };
@@ -26,7 +25,6 @@ async function render(pageContext: PageContextServer) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
