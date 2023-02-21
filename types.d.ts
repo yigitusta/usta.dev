db83489d98d45482a2b25b7a8ad2dc511f4b1707
usta <yigitusta8@gmail.com>
2023-02-22 05:50:43 +0900

    compress images

---
+++ types.d.ts
@@ -0,0 +1,4 @@
+declare module "vite-plugin-image-optimizer" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export function ViteImageOptimizer(any?: any): void;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 import { UserConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 const config: UserConfig = {
-  plugins: [react(), ssr({ prerender: true })],
+  plugins: [react(), ssr({ prerender: true }), ViteImageOptimizer()],
 };
 
 export default config;
