8f0011903622bc5bd5ca48c7a658c3f5e6ce54d1
usta <yigitusta8@gmail.com>
2023-09-06 16:16:58 +0900

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({ forward: ["dataLayer.push"] }),
+  ],
 });
